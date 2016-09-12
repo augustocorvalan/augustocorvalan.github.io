30ff27b55ef0741c51491184dc0e5c6f8e6c2906
Augusto Corvalan <augustocorvalan@gmail.com>
2016-09-12 13:12:49 -0400

    add production flags to webpack

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -14,6 +14,11 @@ module.exports = {
   },
 
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
     new ExtractTextPlugin('style.css', { allChunks: true })
   ],
 
