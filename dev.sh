b3a2b7e3ccb0e75e54d34859ac0ae28935b5bcf1
Augusto Corvalan <augustocorvalan@gmail.com>
2026-03-28 21:11:32 -0400

    Refactor dev.sh to build CSS before starting Tailwind watcher

--- dev.sh
+++ dev.sh
@@ -4,8 +4,11 @@ set -e
 # Kill the background Tailwind watcher when this script exits (Ctrl+C or otherwise)
 trap 'kill "$TAILWIND_PID" 2>/dev/null' EXIT INT TERM
 
-echo "Starting Tailwind CSS watcher..."
+echo "Building CSS..."
 mkdir -p static/css
+./bin/tailwindcss -i assets/css/input.css -o static/css/main.css --minify
+
+echo "Starting Tailwind CSS watcher..."
 ./bin/tailwindcss -i assets/css/input.css -o static/css/main.css --watch &
 TAILWIND_PID=$!
 
